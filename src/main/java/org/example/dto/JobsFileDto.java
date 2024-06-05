335998484e1368fe854d713b05cc4a1a1dc0116d
Thekra Alanezi <thekra.1422@outlook.com>
2024-06-05 11:26:22 +0300

    Add Day7

---
+++ src/main/java/org/example/dto/JobsFileDto.java
@@ -0,0 +1,36 @@
+package org.example.dto;
+
+import jakarta.ws.rs.QueryParam;
+
+public class JobsFileDto
+{
+
+   private @QueryParam("minSalary")Double minSalary ;
+    private  @QueryParam("limit")Integer limit;
+        private @QueryParam("offset")int offset;
+
+    public Double getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(Double minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+}
+
